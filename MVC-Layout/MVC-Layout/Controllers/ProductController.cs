39ac873678cd9424a770c859d2b571a32a1d5a77
berkay yıldız <berkayyildiz504@hotmail.com>
2021-01-31 22:45:23 +0300

    .

---
+++ MVC-Layout/MVC-Layout/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using MVC_Layout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Layout.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products  = db.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveProduct(int productId)
+        {
+            Product product = db.Products.Find(productId);
+            if (product!=null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult UpdateProduct(int productId)
+        {
+            var product = db.Products.Find(productId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult UpdateProduct(Product product)
+        {
+            var updatedProduct = db.Products.Find(product.ProductID);
+            updatedProduct.ProductName = product.ProductName;
+            updatedProduct.Category = product.Category;
+            updatedProduct.UnitPrice = product.UnitPrice;
+            updatedProduct.UnitsInStock = product.UnitsInStock;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVC-Layout/MVC-Layout/Controllers/SupplierController.cs
@@ -0,0 +1,62 @@
+﻿using MVC_Layout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Layout.Controllers
+{
+    public class SupplierController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            return View(db.Suppliers.ToList());
+        }
+        public ActionResult AddSupplier()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddSupplier(Supplier supplier)
+        {
+            db.Suppliers.Add(supplier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveSupplier(int supplierId)
+        {
+            var supplier = db.Suppliers.Find(supplierId);
+            if (supplier!=null)
+            {
+                db.Suppliers.Remove(supplier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult UpdateSupplier(int supplierId)
+        {
+            var supplier = db.Suppliers.Find(supplierId);
+            return View(supplier);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSupplier(Supplier supplier)
+        {
+            var updated = db.Suppliers.Find(supplier.SupplierID);
+            updated.CompanyName = supplier.CompanyName;
+            updated.ContactName = supplier.ContactName;
+            updated.ContactTitle = supplier.ContactTitle;
+            updated.Address = supplier.Address;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
